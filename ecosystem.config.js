28aeb3d3f2f635df14f1ebce6b21f81dd033b9a4
nguoiran2000 <nguoiran2000>
2018-07-27 02:26:14 +0700

    update deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:nguoiran2000/dethithpt.git',
       path : '/var/www/production',
-      'post-deploy' : './resources/pre-deployment.sh && pm2 startOrRestart ecosystem.config.js --env production'
+      'post-deploy' : './resources/pre-deployment.sh && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
