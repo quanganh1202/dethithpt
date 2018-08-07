987067b2a6c24f55428e6f1e31c548ecf72cf204
Vũ Anh Dũng <dungva1505@gmail.com>
2018-08-08 00:35:00 +0700

    DONE AUTH AND REGISTER

--- backend/server/router/index.js
+++ backend/server/router/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { login } from '../../src/controller/user';
+import { tokenGenerator } from '../middleware/jwt';
+import { login, auth, addUser, getAllUsers } from '../../src/controller/user';
 
 const routerDefine =  function defineRouter() {
   const route = express.Router();
@@ -16,6 +17,47 @@ const routerDefine =  function defineRouter() {
         error,
       });
     }
+  });
+
+  route.post('/login/auth/facebook', async (req, res) => {
+    const { email } = req.body;
+    const { token, error } = await auth(email);
+    if (token) {
+      res.status(200).json({
+        token,
+      });
+    } else {
+      res.status(error.status || 401).json({
+        error,
+      });
+    }
+  });
+
+  route.post('/register', async (req, res) => {
+    const userInfo = req.body;
+    const { error, status } = await addUser(userInfo);
+
+    if (error) {
+      res.status(status || 500).json({
+        error: error || 'Unexpected error',
+      });
+    } else {
+      const sign = {
+        role: userInfo.email,
+      };
+      const { token, expiresIn } = tokenGenerator(sign);
+      res.status(status || 201).json({
+        data: 'Has registered',
+        token,
+        expiresIn,
+      });
+    }
+  });
+
+  route.get('/users', async (req, res) => {
+    const users = await getAllUsers();
+
+    res.json(users);
   });
 
   return route;
--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -28,4 +28,51 @@ async function login(userName, pwd) {
   }
 }
 
-export { login };
+async function auth(email) {
+  try {
+    const criteria = { email };
+    const user = await userModel.getList(criteria);
+    if (user && user.length) return tokenGenerator();
+    else return {
+      error: 'Unauthorize',
+      status: 401,
+    };
+  } catch (ex) {
+    return {
+      error: 'Unexpected error when authenticate user',
+      status: 500,
+    };
+  }
+}
+
+async function addUser(userInfo) {
+  try {
+    const { email } = userInfo;
+    const user = await userModel.getList({ email });
+    if (user && user.length) {
+
+      return {
+        error: 'User email already existed',
+        status: 400,
+      };
+    }
+    await userModel.addNewUser(userInfo);
+
+    return {
+      status: 201,
+    };
+  } catch (ex) {
+    return {
+      error: 'Unexpected error when insert an user',
+      status: 500,
+    };
+  }
+}
+
+async function getAllUsers() {
+  const users = await userModel.getList();
+
+  return users;
+}
+
+export { login, auth, addUser, getAllUsers };
