883fdaf4eb1c36b4652df92a8f43d915aee52be3
Vũ Anh Dũng <dungva1505@gmail.com>
2018-08-26 21:46:42 +0700

    Update logger

--- backend/api-command/app/src/libs/logger.js
+++ backend/api-command/app/src/libs/logger.js
@@ -1,45 +1,43 @@
-import * as winston from 'winston';
-import * as path from 'path';
+import winston, { format } from 'winston';
+import path from 'path';
 import moment from 'moment';
+const { combine, timestamp, printf } = format;
+
+const myFormat = printf(info => {
+  return `[${info.timestamp}][${info.level.toUpperCase()}]${info.message}`;
+});
 
 const log = winston.createLogger({
+  format: combine(
+    timestamp(),
+    myFormat
+  ),
   transports: [
     // info console log
     new (winston.transports.Console)({
       level: 'info',
       name: 'info-console',
       colorize: true,
-      timestamp: () => moment().format('YYYY-MM-DD HH-mm-ss'),
-      formatter: winston.format.simple(),
-      json: false,
     }),
     // info log file
     new (winston.transports.File)({
       level: 'info',
       name: 'info-file',
-      filename: path.resolve(__dirname, '../../', 'logs',
+      filename: path.resolve(__dirname, '../../../', 'logs',
         `${moment().format('YYYYMMDD')}-development-info.log`),
-      timestamp: () => moment().format('YYYY-MM-DD HH-mm-ss'),
-      formatter: options => `[${options.timestamp()}]: ${options.message ||''}`,
-      json: false,
     }),
     // errors console log
     new (winston.transports.Console)({
       level: 'error',
       name: 'error-console',
       colorize: true,
-      timestamp: () => moment().format('YYYY-MM-DD HH-mm-ss'),
-      formatter: options => `[${options.timestamp()}]: ${options.message || ''}`,
     }),
     // errors log file
     new (winston.transports.File)({
       level: 'error',
       name: 'error-file',
-      filename: path.resolve(__dirname, '../../', 'logs',
+      filename: path.resolve(__dirname, '../../../', 'logs',
         `${moment().format('YYYYMMDD')}-development-error.log`),
-      timestamp: () => moment().format('YYYY-MM-DD HH-mm-ss'),
-      formatter: options => `[${options.timestamp()}]: ${options.message || ''}`,
-      json: false,
     }),
   ],
 });
