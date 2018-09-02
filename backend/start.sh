014ee8e2cb4e66442c00459aa158c4e133716e15
nguoiran2000 <nguoiran2000>
2018-09-02 12:21:10 +0700

    update docker

---
+++ backend/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /api-command
+npm install
+cd /api-command
+npm run init
+cd /api-command
+npm run updateDb
+cd /api-command
+npm start
+
+
+
+cd /api-query
+npm install
+cd /api-query
+npm run init
+cd /api-query
+npm run updateDb
+cd /api-query
+npm start
