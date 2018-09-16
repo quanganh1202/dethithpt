46801f3754493dad60fc3ac112379d7f0de18a56
nguoiran2000 <nguoiran2000>
2018-09-16 16:51:34 +0700

    docker

--- backend/api-query/resource/init_elasticsearch.sh
+++ backend/api-query/resource/init_elasticsearch.sh
@@ -10,35 +10,35 @@ echo "------------------------------------------------------"
 echo
 
 # Init document
-curl -X PUT "$ES_HOST/documents" -H 'Content-Type: application/json' -d @'./resource/document.mapping.json'
+curl -X PUT "elasticsearch/documents" -H 'Content-Type: application/json' -d @'./resource/document.mapping.json'
 echo
 
 # Init user
-curl -X PUT "$ES_HOST/users" -H 'Content-Type: application/json' -d @'./resource/user.mapping.json'
+curl -X PUT "elasticsearch/users" -H 'Content-Type: application/json' -d @'./resource/user.mapping.json'
 echo
 
 # Init category
-curl -X PUT "$ES_HOST/categories" -H 'Content-Type: application/json' -d @'./resource/category.mapping.json'
+curl -X PUT "elasticsearch/categories" -H 'Content-Type: application/json' -d @'./resource/category.mapping.json'
 echo
 
 # Init tag
-curl -X PUT "$ES_HOST/tags" -H 'Content-Type: application/json' -d @'./resource/tag.mapping.json'
+curl -X PUT "elasticsearch/tags" -H 'Content-Type: application/json' -d @'./resource/tag.mapping.json'
 echo
 
 # Init class
-curl -X PUT "$ES_HOST/classes" -H 'Content-Type: application/json' -d @'./resource/class.mapping.json'
+curl -X PUT "elasticsearch/classes" -H 'Content-Type: application/json' -d @'./resource/class.mapping.json'
 echo
 
 # Init subject
-curl -X PUT "$ES_HOST/subjects" -H 'Content-Type: application/json' -d @'./resource/subject.mapping.json'
+curl -X PUT "elasticsearch/subjects" -H 'Content-Type: application/json' -d @'./resource/subject.mapping.json'
 echo
 
 # Init collection
-curl -X PUT "$ES_HOST/collections" -H 'Content-Type: application/json' -d @'./resource/collection.mapping.json'
+curl -X PUT "elasticsearch/collections" -H 'Content-Type: application/json' -d @'./resource/collection.mapping.json'
 echo
 
 # Init history
-curl -X PUT "$ES_HOST/histories" -H 'Content-Type: application/json' -d @'./resource/history.mapping.json'
+curl -X PUT "elasticsearch/histories" -H 'Content-Type: application/json' -d @'./resource/history.mapping.json'
 echo
 
 echo 'DONE !';
