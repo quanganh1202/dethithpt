51ccd0ad0d1ddcdb5d2b3759c0ffea2adb2ce608
nguoiran2000 <nguoiran2000>
2018-09-16 16:56:05 +0700

    docker

--- backend/api-query/resource/init_elasticsearch.sh
+++ backend/api-query/resource/init_elasticsearch.sh
@@ -10,35 +10,35 @@ echo "------------------------------------------------------"
 echo
 
 # Init document
-curl -X PUT "103.92.29.145:9200/documents" -H 'Content-Type: application/json' -d @'./resource/document.mapping.json'
+curl -X PUT "$ES_HOST/documents" -H 'Content-Type: application/json' -d @'./resource/document.mapping.json'
 echo
 
 # Init user
-curl -X PUT "103.92.29.145:9200/users" -H 'Content-Type: application/json' -d @'./resource/user.mapping.json'
+curl -X PUT "$ES_HOST/users" -H 'Content-Type: application/json' -d @'./resource/user.mapping.json'
 echo
 
 # Init category
-curl -X PUT "103.92.29.145:9200/categories" -H 'Content-Type: application/json' -d @'./resource/category.mapping.json'
+curl -X PUT "$ES_HOST/categories" -H 'Content-Type: application/json' -d @'./resource/category.mapping.json'
 echo
 
 # Init tag
-curl -X PUT "103.92.29.145:9200/tags" -H 'Content-Type: application/json' -d @'./resource/tag.mapping.json'
+curl -X PUT "$ES_HOST/tags" -H 'Content-Type: application/json' -d @'./resource/tag.mapping.json'
 echo
 
 # Init class
-curl -X PUT "103.92.29.145:9200/classes" -H 'Content-Type: application/json' -d @'./resource/class.mapping.json'
+curl -X PUT "$ES_HOST/classes" -H 'Content-Type: application/json' -d @'./resource/class.mapping.json'
 echo
 
 # Init subject
-curl -X PUT "103.92.29.145:9200/subjects" -H 'Content-Type: application/json' -d @'./resource/subject.mapping.json'
+curl -X PUT "$ES_HOST/subjects" -H 'Content-Type: application/json' -d @'./resource/subject.mapping.json'
 echo
 
 # Init collection
-curl -X PUT "103.92.29.145:9200/collections" -H 'Content-Type: application/json' -d @'./resource/collection.mapping.json'
+curl -X PUT "$ES_HOST/collections" -H 'Content-Type: application/json' -d @'./resource/collection.mapping.json'
 echo
 
 # Init history
-curl -X PUT "103.92.29.145:9200/histories" -H 'Content-Type: application/json' -d @'./resource/history.mapping.json'
+curl -X PUT "$ES_HOST/histories" -H 'Content-Type: application/json' -d @'./resource/history.mapping.json'
 echo
 
 echo 'DONE !';
