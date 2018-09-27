14afef989d605fb8bba5f96fb2df80d98b416c54
nguoiran2000 <nguoiran2000>
2018-09-27 22:36:09 +0700

    fix bug upload file multi

--- frontend/app/containers/UploadDocument/DetailForm.js
+++ frontend/app/containers/UploadDocument/DetailForm.js
@@ -259,7 +259,7 @@ class DetailForm extends React.Component {
           </div>
         ) : null}
         {
-          (this.props.name.split('.')[1] === 'zip' || this.props.name.split('.')[1] === 'rar') ?
+          (this.props.name && (this.props.name.split('.')[1] === 'zip' || this.props.name.split('.')[1] === 'rar')) ?
           <div className="form-group">
             <label htmlFor="cateIds">&nbsp;</label>
             <input
