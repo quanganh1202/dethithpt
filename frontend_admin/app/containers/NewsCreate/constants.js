e4007579f83b385b4939370d26c0038b8c3477cf
quanganh <quanganh1202@gmail.com>
2018-10-01 21:09:10 +0700

    Add admin NEWS

---
+++ frontend_admin/app/containers/NewsCreate/constants.js
@@ -0,0 +1,15 @@
+/*
+ * HomeConstants
+ * Each action has a corresponding type, which the reducer knows and picks up on.
+ * To avoid weird typos between the reducer and the actions, we save them as
+ * constants here. We prefix them with 'yourproject/YourComponent' so we avoid
+ * reducers accidentally picking up actions they shouldn't.
+ *
+ * Follow this format:
+ * export const YOUR_ACTION_CONSTANT = 'yourproject/YourContainer/YOUR_ACTION_CONSTANT';
+ */
+import { defineAction } from 'redux-define';
+
+const container = 'thpt/newsCreate';
+export const CREATE_NEWS = defineAction('CREATE_NEWS', ['REQUEST', 'SUCCESS', 'FAILURE'], container);
+export const CLEAR_MESSAGE = defineAction('CLEAR_MESSAGE', container);
