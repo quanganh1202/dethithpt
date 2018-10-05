a25a5960a2d03fe70c46d4dda7611df4e6a5660c
nguoiran2000 <nguoiran2000>
2018-10-06 01:32:22 +0700

    update generate image

--- backend/api-command/app/src/libs/helper.js
+++ backend/api-command/app/src/libs/helper.js
@@ -127,7 +127,8 @@ const preview = function getPreview(fileName) {
 
 const pdf2Image = (pdf, image) => {
   return new Promise((resolve, reject) => {
-    gm('img.png').command('convert').in('+adjoin').quality(100).in(pdf).write(image, function(err) {
+    // gm('img.png').command('convert').in('+adjoin').quality(100).in(pdf).write(image, function(err) {
+    gm().command('convert').in('+adjoin').quality(100).flatten().density(170, 170).in(pdf).write(image, function(err) {
       if(err) return reject(err);
 
       return resolve();
