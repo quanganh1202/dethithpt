f68125a1d484cd254f5725764a684860bc0136a0
Anh Pham <quanganh1202@gmail.com>
2018-10-07 18:35:32 +0700

    Add collection list

---
+++ frontend/app/containers/Collection/selectors.js
@@ -0,0 +1,28 @@
+/**
+ * Homepage selectors
+ */
+
+import { createSelector } from 'reselect';
+import { initialState } from './reducer';
+
+const selectHome = state => state.get('collection', initialState);
+
+const makeSelectDocument = () =>
+  createSelector(selectHome, categoryState => categoryState.get('document').toJS());
+
+const makeSelectLoading = () =>
+  createSelector(selectHome, categoryState => categoryState.get('loading'));
+
+const makeSelectDocuments = () =>
+  createSelector(selectHome, categoryState => categoryState.get('documents').toJS());
+
+const makeSelectFilterData = () =>
+  createSelector(selectHome, categoryState => categoryState.get('filterData').toJS());
+
+export {
+  selectHome,
+  makeSelectDocument,
+  makeSelectLoading,
+  makeSelectDocuments,
+  makeSelectFilterData,
+};
