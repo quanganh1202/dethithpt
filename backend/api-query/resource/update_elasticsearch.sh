d73f5051dae87fe32567cbb70055d30cb8732428
Vũ Anh Dũng <dungva1505@gmail.com>
2018-10-23 23:31:57 +0700

    Updated

--- backend/api-query/resource/update_elasticsearch.sh
+++ backend/api-query/resource/update_elasticsearch.sh
@@ -2,14 +2,14 @@ curl -X DELETE "localhost:9200/users"
 
 curl -X PUT "localhost:9200/users" -H 'Content-Type: application/json' -d @'./resource/user.mapping.json'
 
-curl -X POST "$ES_HOST:9200/users/user/1?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"vuanhdung.khmt2k7@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
+curl -X POST "localhost:9200/users/user/1?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"vuanhdung.khmt2k7@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
 
-curl -X POST "$ES_HOST:9200/users/user/2?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"vuanhdung.khmt2@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
+curl -X POST "localhost:9200/users/user/2?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"vuanhdung.khmt2@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
 
-curl -X POST "$ES_HOST:9200/users/user/3?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"quanganh1202@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
+curl -X POST "localhost:9200/users/user/3?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"quanganh1202@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
 
-curl -X POST "$ES_HOST:9200/users/user/4?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"tdgalaxycorp@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
+curl -X POST "localhost:9200/users/user/4?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"tdgalaxycorp@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
 
-curl -X POST "$ES_HOST:9200/users/user/5?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"phamdung282@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
+curl -X POST "localhost:9200/users/user/5?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"phamdung282@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
 
-curl -X POST "$ES_HOST:9200/users/user/6?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"nguoiran2000@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
+curl -X POST "localhost:9200/users/user/6?op_type=create" -H 'Content-Type: application/json' -d '{"name":"administator","email":"nguoiran2000@gmail.com","role":"admin","status": 2,"numOfDownloaded": 0,"numOfUploaded": 0, "money": 0}'
